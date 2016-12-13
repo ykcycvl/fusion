e2d176a39fa652794293a38bc18e6756736ed073
JustJoni <just-joni6@mail.ru>
2016-12-13 18:03:37 +1000

    Добавил проверку на изменение полей
    Код между //%%%%%%%

--- Fusion/Controllers/CallbackController.cs
+++ Fusion/Controllers/CallbackController.cs
@@ -177,70 +177,349 @@ namespace Fusion.Controllers
         {
             #region UserData
 
+            //переменная для записи изменённых полей
+            string Matching=""; 
+
             int id;
             //id = model.id;
             id = Convert.ToInt32(Request.Form["id"]);
-            if (id == 0)
+            if(id==0)
             {
                 id = model.id;
             }
-
+            
             string UserName;
             UserName = model.UserName;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string UserNamematching;
+            UserNamematching = Request.Form["hidname"];
+            //проверяем, было ли поле изменено
+            if(UserName!=UserNamematching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Имя гостя, ";
+            }
+            //%%%%%%%
 
             string phnumber;
             phnumber = model.phnumber;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string phnumbermatching;
+            phnumbermatching = Request.Form["hidphone"];
+            //проверяем, было ли поле изменено
+            if (phnumber != phnumbermatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Номер тел., ";
+            }
+            //%%%%%%%
 
             string email;
             email = model.email;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string emailmatching;
+            emailmatching = Request.Form["hidmail"];
+            //проверяем, было ли поле изменено
+            if (email != emailmatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Email,";
+            }
+            //%%%%%%%
 
             string SelectedUnit;
             SelectedUnit = Request.Form["unitt"];
-
+            //%%%%%%%
+            //переменная для начального значения поля
+            string SelectedUnitmatching;
+            SelectedUnitmatching = Request.Form["hidunit"];
+            //проверяем, было ли поле изменено
+            if (SelectedUnit != SelectedUnitmatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Источник, ";
+            }
+            //%%%%%%%
+            
             //string SelectedUnit; test
             //SelectedUnit = model.SelectedUnit;unit
 
             string Problem;
             Problem = model.Problem;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string Problemmatching;
+            Problemmatching = Request.Form["hidproblem"];
+            //проверяем, было ли поле изменено
+            if (Problem != Problemmatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Подробности проблемы, ";
+            }
+            //%%%%%%%
 
             string SelectedRest;
             SelectedRest = Request.Form["restt"];
+            //%%%%%%%
+            //переменная для начального значения поля
+            string SelectedRestmatching;
+            SelectedRestmatching = Request.Form["hidrest"];
+            //проверяем, было ли поле изменено
+            if (SelectedRest != SelectedRestmatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Ресторан, ";
+            }
+            //%%%%%%%
             //string SelectedRest;
             //SelectedRest = model.SelectedRest;
 
             string Staff;
             Staff = model.Staff;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string Staffmatching;
+            Staffmatching = Request.Form["hidstaff"];
+            //проверяем, было ли поле изменено
+            if (Staff != Staffmatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Ответственный, ";
+            }
+            //%%%%%%%
 
             string SelectedSource;
             SelectedSource = Request.Form["sourcee"];
+            //%%%%%%%
+            //переменная для начального значения поля
+            string SelectedSourcematching;
+            SelectedSourcematching = Request.Form["hidsourse"];
+            //проверяем, было ли поле изменено
+            if (SelectedSource != SelectedSourcematching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Источник, ";
+            }
+            //%%%%%%%
             //string SelectedSource;
             //SelectedSource = model.SelectedSource;            
 
             string NewDate;
             NewDate = model.NewDate;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string NewDatematching;
+            NewDatematching = Request.Form["hiddatenew"];
+            //проверяем, было ли поле изменено
+            if (NewDate != NewDatematching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Дата создания, ";
+            }
+            //%%%%%%%
 
             string NewTime;
             NewTime = model.NewTime;
-            NewDate = NewDate + " " + NewTime;
-
+            NewDate = NewDate +" "+ NewTime;
+            
             //string RegDateNew = Regex.Replace(test, patternDate, test);
 
             string OldDate;
             OldDate = model.OldDate;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string OldDatematching;
+            OldDatematching = Request.Form["hiddateold"];
+            //проверяем, было ли поле изменено
+            if (OldDate != OldDatematching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Дата закрытия, ";
+            }
+            //%%%%%%%
 
             //string OldDate;
             //OldDate = OldDate + " " + OldTime;
-
+            
             string Guilty;
             Guilty = model.Guilty;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string Guiltymatching;
+            Guiltymatching = Request.Form["hidguilty"];
+            //проверяем, было ли поле изменено
+            if (Guilty != Guiltymatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Виновный, ";
+            }
+            //%%%%%%%
 
             string SelectedPayer;
             SelectedPayer = Request.Form["payerr"];
+            //%%%%%%%
+            //переменная для начального значения поля
+            string SelectedPayermatching;
+            SelectedPayermatching = Request.Form["hidpayer"];
+            //проверяем, было ли поле изменено
+            if (SelectedPayer != SelectedPayermatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Кто заплатил, ";
+            }
+            //%%%%%%%
 
             string Rating;
             //Rating = model.Rating1;
             Rating = Request.Form["Rating1"];
             string pattern = @"\w+,+";
+            string regular = "";
+            Rating = Regex.Replace(Rating, pattern, regular);
+
+
+            switch(Rating)
+            {
+                case ",Положительный":
+                    Rating = "Положительный";
+                    break;
+                case ",Отрицательный":
+                    Rating = "Отрицательный";
+                    break;
+                case ",Критичный":
+                    Rating = "Критичный";
+                    break;
+                case ",ОТРАВЛЕНИЕ":
+                    Rating = "ОТРАВЛЕНИЕ";
+                    break;
+            }
+            //%%%%%%%
+            //переменная для начального значения поля
+            string Ratingmatching;
+            Ratingmatching = Request.Form["hidrating1"];
+            //проверяем, было ли поле изменено
+            if (Rating != Ratingmatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Оценка, ";
+            }
+            //%%%%%%%
+
+            string SelectedRating2;
+            SelectedRating2 = Request.Form["reitingg"];
+            //%%%%%%%
+            //переменная для начального значения поля
+            string SelectedRating2matching;
+            SelectedRating2matching = Request.Form["hidrating2"];
+            //проверяем, было ли поле изменено
+            if (SelectedRating2 != SelectedRating2matching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Оценка2, ";
+            }
+            //%%%%%%%
+            //string SelectedRating2;
+            //SelectedRating2 = model.SelectedRating2;
+
+            string textkomm;
+            textkomm = model.textkomm;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string textkommmatching;
+            textkommmatching = Request.Form["hidtext"];
+            //проверяем, было ли поле изменено
+            if (textkomm != textkommmatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Текст, ";
+            }
+            //%%%%%%%
+
+            string mera;
+            mera = model.mera;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string meramatching;
+            meramatching = Request.Form["hidmera"];
+            //проверяем, было ли поле изменено
+            if (mera != meramatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Меры, ";
+            }
+            //%%%%%%%
+
+            string answer;
+            answer = model.answer;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string answermatching;
+            answermatching = Request.Form["hidanswer"];
+            //проверяем, было ли поле изменено
+            if (answer != answermatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Решение для гостя, ";
+            }
+            //%%%%%%%
+
+            string Cost;
+            Cost = model.Cost;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string Costmatching;
+            Costmatching = Request.Form["hidcost1"];
+            //проверяем, было ли поле изменено
+            if (Cost != Costmatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Цена в руб., ";
+            }
+            //%%%%%%%
+
+            string CostPoint;
+            CostPoint = model.CostPoint;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string CostPointmatching;
+            CostPointmatching = Request.Form["hidcost2"];
+            //проверяем, было ли поле изменено
+            if (CostPoint != CostPointmatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Цена в баллах, ";
+            }
+            //%%%%%%%
+
+            string CostSert;
+            CostSert = model.CostSert;
+            //%%%%%%%
+            //переменная для начального значения поля
+            string CostSertmatching;
+            CostSertmatching = Request.Form["hidcost3"];
+            //проверяем, было ли поле изменено
+            if (CostSert != CostSertmatching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Цена сертификатов, ";
+            }
+            //%%%%%%%
+
+            string Type;
+            Type = Request.Form["types"];
+            //%%%%%%%
+            //переменная для начального значения поля
+            string Typematching;
+            Typematching = Request.Form["hidtype"];
+            //проверяем, было ли поле изменено
+            if (Type != Typematching)
+            {
+                //если изменено - записываем в переменную какое поле было изменено
+                Matching = Matching + "Тип причины, ";
+            }
+            //%%%%%%%
             string regular = "";
             Rating = Regex.Replace(Rating, pattern, regular);
 
