2cb6bc339fc980077d151220d586492e23f39be0
ykcycvl <ykcycvl@gmail.com>
2017-03-14 13:35:41 +1000

    !ActiveDirectoryUserFullName
    FullName instead of login

--- Fusion/Models/AccountViewModels.cs
+++ Fusion/Models/AccountViewModels.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.DirectoryServices;
 
 public class LoginViewModel
 {
@@ -13,4 +14,25 @@ public class LoginViewModel
 
     [Display(Name = "Запомнить?")]
     public bool RememberMe { get; set; }
+
+    public static string GetUserProperty(string accountName, string propertyName)
+    {
+        DirectoryEntry entry = new DirectoryEntry();
+        entry.Path = "LDAP://fg.local:389/DC=fg,DC=local";
+        entry.AuthenticationType = AuthenticationTypes.Secure;
+
+        DirectorySearcher search = new DirectorySearcher(entry);
+        search.Filter = "(SAMAccountName=" + accountName + ")";
+        search.PropertiesToLoad.Add(propertyName);
+
+        SearchResultCollection results = search.FindAll();
+        if (results != null && results.Count > 0)
+        {
+            return results[0].Properties[propertyName][0].ToString();
+        }
+        else
+        {
+            return "Unknown User";
+        }
+    }
 }
