3a13a41c03b73afda4199c4a16ae0c5287f6a07d
ykcycvl <ykcycvl@gmail.com>
2017-04-04 09:31:50 +1000

    +

--- Fusion/Models/SalaryModels.cs
+++ Fusion/Models/SalaryModels.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using V83;
 using System.Web.Configuration;
+using System.Reflection;
 
 namespace Fusion.Models
 {
     public class SalaryModels
     {
+        public class HourPerDay
+        {
+            public DateTime Day { get; set; }
+            public Decimal Hours { get; set; }
+        }
         public class Detention
         {
             public string Code { get; set; }
@@ -23,6 +29,7 @@ namespace Fusion.Models
             public string FullName { get; set; }
             public string Position { get; set; }
             public List<Detention> Detentions { get; set; }
+            public List<HourPerDay> TimeSheet { get; set; }
         }
 
         public class Subdivision
@@ -37,6 +44,10 @@ namespace Fusion.Models
                 FullName = fullname;
                 Employees = new List<Employee>();
             }
+            public Subdivision()
+            {
+                Employees = new List<Employee>();
+            }
         }
 
         public class Organization
@@ -44,7 +55,8 @@ namespace Fusion.Models
             string connectionString = WebConfigurationManager.ConnectionStrings["ZupConnectionString"].ConnectionString;
             public string FullName { get; set; }
             public List<Subdivision> Subdivisions { get; set; }
-            public List<Detention> Detentions;
+            public List<Detention> Detentions { get; set; }
+            public DateTime Period { get; set; }
             public void GetFullData()
             {
                 //Список возможных удержаний из 1С
@@ -67,7 +79,14 @@ namespace Fusion.Models
                         Subdivisions.Add(s);
                     }
 
-                    s.Employees.Add(new Employee() { Code = res.Сотрудник.Код, FullName = res.Сотрудник.Наименование, Detentions = Detentions, Position = res.Должность.Наименование });
+                    Employee e = new Employee() { Code = res.Сотрудник.Код, FullName = res.Сотрудник.Наименование, Detentions = Detentions, Position = res.Должность.Наименование, TimeSheet = new List<HourPerDay>() };
+
+                    for (int i = 1; i <= DateTime.DaysInMonth(Period.Year, Period.Month); i++)
+                    {
+                        e.TimeSheet.Add(new HourPerDay() { Day = new DateTime(Period.Year, Period.Month, i), Hours = 0 });
+                    }
+
+                    s.Employees.Add(e);
                 }
 
                 connection = false;
@@ -122,6 +141,111 @@ namespace Fusion.Models
                 }
 
                 return Detentions;
+            }
+
+            public void Post()
+            {
+                try
+                {
+                    COMConnector connector = new COMConnector();
+                    dynamic connection = connector.Connect(connectionString);
+                    try
+                    {
+                        /* Удержания */
+                        dynamic doc = connection.Документы.НачислениеЗарплаты.СоздатьДокумент();
+                        doc.Организация = connection.Справочники.Организации.НайтиПоНаименованию(FullName);
+                        doc.МесяцНачисления = Period.ToString("yyyyMM01");
+                        doc.РежимДоначисления = false;
+                        doc.Дата = DateTime.Today.ToString("yyyyMMdd");
+
+                        for (int i = 0; i < Subdivisions.Count; i++)
+                        {
+                            for (int j = 0; j < Subdivisions[i].Employees.Count; j++)
+                            {
+                                for (int k = 0; k < Subdivisions[i].Employees[j].Detentions.Count; k++)
+                                {
+                                    if (Subdivisions[i].Employees[j].Detentions[k].Sum > 0)
+                                    {
+                                        dynamic det = doc.Удержания.Добавить();
+                                        det.ФизическоеЛицо = connection.Справочники.Сотрудники.НайтиПоКоду(Subdivisions[i].Employees[j].Code).ФизическоеЛицо;
+                                        det.Удержание = connection.ПланыВидовРасчета.Удержания.НайтиПоКоду(Subdivisions[i].Employees[j].Detentions[k].Code);
+                                        det.ДатаНачала = Period.ToString("yyyyMM01");
+                                        det.ДатаОкончания = Period.ToString("yyyyMM") + DateTime.DaysInMonth(Period.Year, Period.Month).ToString();
+                                        det.Результат = Subdivisions[i].Employees[j].Detentions[k].Sum;
+                                        det.Сотрудник = connection.Справочники.Сотрудники.НайтиПоКоду(Subdivisions[i].Employees[j].Code);
+                                    }
+                                }
+                            }
+                        }
+
+                        doc.Записать(connection.РежимЗаписиДокумента.Проведение);
+
+                        /* Конец Удержания*/
+
+                        doc = null;
+
+                        /* Индивидуальные графики */
+
+                        doc = connection.Документы.ИндивидуальныйГрафик.СоздатьДокумент();
+                        doc.ПериодРегистрации = Period.ToString("yyyyMM01");
+                        doc.Организация = connection.Справочники.Организации.НайтиПоНаименованию(FullName);
+                        doc.ДатаНачалаПериода = Period.ToString("yyyyMM01");
+                        doc.ДатаОкончанияПериода = new DateTime(Period.Year, Period.Month, DateTime.DaysInMonth(Period.Year, Period.Month)).ToString("yyyyMMdd");
+                        doc.ПериодВводаДанныхОВремени = connection.Перечисления.ПериодыВводаДанныхОВремени.ТекущийМесяц;
+                        doc.Дата = DateTime.Today.ToString("yyyyMMdd");
+
+                        for (int i = 0; i < Subdivisions.Count; i++)
+                        {
+                            for (int j = 0; j < Subdivisions[i].Employees.Count; j++)
+                            {
+                                Decimal total = Subdivisions[i].Employees[j].TimeSheet.Sum(p => p.Hours);
+
+                                if (total > 0)
+                                {
+                                    dynamic dot = doc.ДанныеОВремени.Добавить();
+
+                                    for (int k = 0; k < Subdivisions[i].Employees[j].TimeSheet.Count; k++)
+                                    {
+                                        if (Subdivisions[i].Employees[j].TimeSheet[k].Hours > 0)
+                                        {
+                                            dynamic Str = connection.NewObject("Структура");
+                                            Str.Вставить("Сотрудник", connection.Справочники.Сотрудники.НайтиПоКоду(Subdivisions[i].Employees[j].Code));
+                                            Str.Вставить("Часов" + Subdivisions[i].Employees[j].TimeSheet[k].Day.Day.ToString(), Subdivisions[i].Employees[j].TimeSheet[k].Hours);
+                                            Str.Вставить("ВидВремени" + Subdivisions[i].Employees[j].TimeSheet[k].Day.Day.ToString(), connection.Справочники.ВидыИспользованияРабочегоВремени.Явка);
+                                            connection.ЗаполнитьЗначенияСвойств(dot, Str);
+                                        }
+                                        else
+                                        {
+                                            dynamic Str = connection.NewObject("Структура");
+                                            Str.Вставить("Сотрудник", connection.Справочники.Сотрудники.НайтиПоКоду(Subdivisions[i].Employees[j].Code));
+                                            Str.Вставить("ВидВремени" + Subdivisions[i].Employees[j].TimeSheet[k].Day.Day.ToString(), connection.Справочники.ВидыИспользованияРабочегоВремени.ВыходныеДни);
+                                            connection.ЗаполнитьЗначенияСвойств(dot, Str);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        doc.Записать(connection.РежимЗаписиДокумента.Проведение);
+                        doc = false;
+                        doc = null;
+
+                        /* Конец Индивидуальные графики */
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                    finally
+                    {
+                        connection = false;
+                        connection = null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
             }
         }
     }
