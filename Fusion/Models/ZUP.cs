6068e6c34d376a776261a089fab6b12d60b1ee07
ykcycvl <ykcycvl@gmail.com>
2017-04-04 09:38:42 +1000

    +SalaryModule

---
+++ Fusion/Models/ZUP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fusion.Models
+{
+    public class ZUP
+    {
+        public class Detention
+        {
+            public string code { get; set; }
+            public string Comment { get; set; }
+            public string DateStart { get; set; }
+            public string DateEnd { get; set; }
+            public string Summ { get; set; }
+        }
+
+        public class Employee
+        {
+            public string code { get; set; }
+
+        }
+    }
+}
