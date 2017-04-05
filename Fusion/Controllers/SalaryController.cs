40e048e03a477a96e97c2c969a8e7beb0795426f
ykcycvl <ykcycvl@gmail.com>
2017-04-05 17:16:46 +1000

    +Accruals

--- Fusion/Controllers/SalaryController.cs
+++ Fusion/Controllers/SalaryController.cs
@@ -4,30 +4,46 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Fusion.Models;
+using System.Web.Configuration;
 
 namespace Fusion.Controllers
 {
     public class SalaryController : Controller
     {
+        string connectionString = WebConfigurationManager.ConnectionStrings["ZupConnectionString"].ConnectionString;
         // GET: Salary
         public ActionResult Index()
         {
-            SalaryModels.Organization model = new SalaryModels.Organization() { FullName = "ФьюжнГрупп" };
-            model.GetFullData();
-            return View(model);
+            return View();
         }
 
         public ActionResult Sheet()
         {
             SalaryModels.Organization model = new SalaryModels.Organization() { FullName = "ФьюжнГрупп" };
-            model.Period = DateTime.Today;
-            model.GetFullData();
+
+            try
+            {
+                model.connection = ((V83.COMConnector)HttpContext.Application["connector"]).Connect(connectionString);
+                model.Period = DateTime.Today;
+                model.GetFullData();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            finally
+            {
+                model.connection = false;
+                model.connection = null;
+            }
+
             return View(model);
         }
 
         [HttpPost]
         public ActionResult Sheet(SalaryModels.Organization model)
         {
+            model.connection = ((V83.COMConnector)HttpContext.Application["connector"]).Connect(connectionString);
             model.Post();
             return View(model);
         }
