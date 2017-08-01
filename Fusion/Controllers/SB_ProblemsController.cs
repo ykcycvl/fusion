706ef97243171ba485ff9dceba09a7c01b4223f4
Arhont <logmir.das@mail.ru>
2017-08-01 14:45:32 +1000

    SB

---
+++ Fusion/Controllers/SB_ProblemsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fusion.Models;
+using Microsoft.AspNet.Identity;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices;
+
+namespace Fusion.Controllers
+{
+    public class SB_ProblemsController : Controller
+    {
+        // GET: SB_Problems
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Problems()
+        {
+            SB model = new SB();
+            model.username = User.Identity.GetUserName();
+            if (model.username == null || model.username == "")
+            {
+                return Redirect("~/Home");
+            }
+            else if (LoginViewModel.IsMemberOf(model.username, "SB_Admin") || LoginViewModel.IsMemberOf(model.username, "SB_User"))
+            {
+                model.getProblems();
+                return View(model);
+            }
+            else return Redirect("~/Home");
+        }
+        [HttpPost]
+        public ActionResult Problems(SB model)
+        {
+            model.username = User.Identity.GetUserName();
+            model.sentProblem();
+            model.getProblems();
+            return View(model);
+        }
+        public ActionResult Restaurants()
+        {
+            SB model = new SB();
+            model.getProblems();
+            return View(model);
+        }
+    }
+}
