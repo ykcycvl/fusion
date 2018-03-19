c64626af26e15b514f849fd292ee1735746bdbfb
ykcycvl <ykcycvl@gmail.com>
2018-03-19 14:14:51 +1000

    +SKK
    Добавлены интерфейсы

---
+++ Fusion/Models/SKKInterfaces.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fusion.Interfaces
+{
+    public interface IReference<T>
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        bool Deleted { get; set; }
+        T Add(T Item);
+        T Edit(T Item);
+        void Delete(int Id);
+    }
+
+    public interface IInspectionReport
+    {
+        DateTime InspectionDate { get; set; }
+        IEmployee Inspector { get; set; }
+        IEmployee Director { get; set; }
+        IEmployee Chef { get; set; }
+        IEmployee SousChef { get; set; }
+        IEmployee HallManager { get; set; }
+        IRestaurant Restaurant { get; set; }
+        List<ICategory> Categories { get; set; }
+        IInspectionReport GetByID(int Id);
+        IInspectionReport Get(int RestaurantID, DateTime InspectionDate);
+        IInspectionReport Save(IInspectionReport Report);
+    }
+
+    public interface IRestaurant : IReference<IRestaurant>
+    {
+        IEmployee Director { get; set; }
+        IEmployee Chef { get; set; }
+    }
+
+    public interface ICategory: IReference<ICategory>
+    {
+        List<IArticle> Articles { get; set; }
+    }
+
+    public interface IArticle: IReference<IArticle>
+    {
+        int Cost { get; set; }
+        int Rating { get; set; }
+        string Comment { get; set; }
+        List<IFile> Files { get; set; }
+        void AddFile(IFile File);
+        void DeleteFile(IFile File);
+    }
+    public interface IFile
+    {
+        string FullFilePath { get; set; }
+        string Description { get; set; }
+    }
+
+    public interface IEmployee: IReference<IEmployee>
+    {
+        IPosition Position { get; set; }
+    }
+
+    public interface IPosition: IReference<IPosition>
+    {
+    }
+}
---
+++ Fusion/Models/SKKModels.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Fusion.Interfaces;
+
+namespace Fusion.Models
+{
+    public class SKKModels
+    {
+        public class Aticle : IArticle
+        {
+            public int Cost { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public int Rating { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string Comment { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public List<IFile> Files { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public bool Deleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public IArticle Add(IArticle Item)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void AddFile(IFile File)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Delete(int Id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void DeleteFile(IFile File)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IArticle Edit(IArticle Item)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class Category : ICategory
+        {
+            public List<IArticle> Articles { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public bool Deleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public ICategory Add(ICategory Item)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Delete(int Id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ICategory Edit(ICategory Item)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class Restaurant : IRestaurant
+        {
+            public IEmployee Director { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public IEmployee Chef { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public bool Deleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public IRestaurant Add(IRestaurant Item)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Delete(int Id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IRestaurant Edit(IRestaurant Item)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class Employee : IEmployee
+        {
+            public IPosition Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public bool Deleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public IEmployee Add(IEmployee Item)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Delete(int Id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEmployee Edit(IEmployee Item)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class Position : IPosition
+        {
+            public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public bool Deleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public IPosition Add(IPosition Item)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Delete(int Id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IPosition Edit(IPosition Item)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class InspectionReport : IInspectionReport
+        {
+            public DateTime InspectionDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public IEmployee Inspector { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public IEmployee Director { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public IEmployee Chef { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public IEmployee SousChef { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public IEmployee HallManager { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public IRestaurant Restaurant { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public List<ICategory> Categories { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public IInspectionReport Get(int RestaurantID, DateTime InspectionDate)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IInspectionReport GetByID(int Id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IInspectionReport Save(IInspectionReport Report)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
